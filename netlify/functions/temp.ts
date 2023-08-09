3a19d673ae9b53870f6d2475df812ee743f78ba3
Kingsley <chan.kingsley@gmail.com>
2023-08-09 13:56:13 -0600

    update temp response

--- netlify/functions/temp.ts
+++ netlify/functions/temp.ts
@@ -3,6 +3,9 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://checklist-ui-1982d5.netlify.app'
+    },
     body: JSON.stringify({ message: "Hello World" }),
   };
 };
