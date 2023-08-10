5330b4561169214fec56a9c5d6bdb851fe5144a0
Kingsley <chan.kingsley@gmail.com>
2023-08-10 10:57:54 -0600

    add expressjs for local development

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,56 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { HandlerEvent } from '@netlify/functions';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-app.get('*', (req: Request, res: Response) => {
-  console.log(333, req.url);
-  const { handler } = require(`${__dirname}/..${req.url}.js`);
-  console.log(444, handler());
-  res.send('Express + TypeScript Server');
+const convertToNetlifyEvent = (req: Request): HandlerEvent => ({
+  httpMethod: req.method,
+  rawUrl: req.originalUrl,
+  rawQuery: '',
+  path: req.path,
+  headers: {},
+  multiValueHeaders: {},
+  queryStringParameters: null,
+  multiValueQueryStringParameters: null,
+  body: req.body,
+  isBase64Encoded: true
+});
+
+app.use(bodyParser.json())
+app.use(cors());
+
+app.all('*', async (req: Request, res: Response) => {
+  try {
+    const { handler } = require(`${__dirname}/../netlify/functions${req.url}.ts`);
+    const { statusCode, headers, body } = await handler(convertToNetlifyEvent(req));
+
+    if (headers) {
+      for (let k in headers) {
+        res.set(k, headers[k]);
+      }
+    }
+
+    if (statusCode) {
+      res.status(statusCode);
+    }
+
+    res.send(body);
+  } catch (exception) {
+    if (exception.code === 'MODULE_NOT_FOUND') {
+      res.status(404);
+      return res.send(exception.code);
+    }
+
+    console.log(exception);
+    res.status(500);
+    res.send(exception.code);
+  }
 });
 
 app.listen(port, () => {
